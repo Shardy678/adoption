dcc07959198552a3b28d4221142e12bd1d0c3c19
Shardy678 <tambov.j678@gmail.com>
2024-09-11 12:33:34 +0300

    move to typescript with vite

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -22,10 +22,10 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs text-4xl text-red-500">
+      <p className="text-3xl font-bold underline">
         Click on the Vite and React logos to learn more
       </p>
     </>
