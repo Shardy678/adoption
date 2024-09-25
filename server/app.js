e68f390127738d497763f9e61f2c9ccb0dea2513
Shardy678 <tambov.j678@gmail.com>
2024-09-25 10:40:55 +0300

    update cors

--- server/app.js
+++ server/app.js
@@ -24,7 +24,7 @@ app.use(
   cors({
     origin:
       process.env.NODE_ENV === 'production'
-        ? 'https://adoption-nosweats-projects.vercel.app/'
+        ? 'https://adoption-one.vercel.app/'
         : 'http://localhost:5173',
   })
 )
