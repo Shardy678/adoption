a1595821d69ab42630d4b175a773ff24131613d4
Shardy678 <tambov.j678@gmail.com>
2024-09-25 09:45:48 +0300

    add vercel.json for api deployment

--- server/app.js
+++ server/app.js
@@ -2,8 +2,8 @@ const express = require('express')
 const connectDB = require('./db')
 const morgan = require('morgan')
 const cors = require('cors')
-const path = require('path')
 const passport = require('passport')
+const serverless = require('serverless-http')
 
 const animalRoutes = require('./routes/animals')
 const adopterRoutes = require('./routes/adopters')
@@ -11,33 +11,38 @@ const adoptionRoutes = require('./routes/adoptions')
 const authRoutes = require('./routes/authRoutes')
 
 const app = express()
-const port = 3000
+
+// Connect to the database
 connectDB()
 
+// Use middleware
 app.use(morgan('dev'))
 app.use(express.json())
+
+// CORS
 app.use(
   cors({
-    origin: 'http://localhost:5173',
+    origin:
+      process.env.NODE_ENV === 'production'
+        ? 'https://adoption-nosweats-projects.vercel.app/'
+        : 'http://localhost:5173',
   })
 )
 
-const clientDistPath = path.join(__dirname, '../client/dist')
-app.use(express.static(clientDistPath))
-
-// Route all other requests to the 'index.html'
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client/dist', 'index.html'))
-})
-
+// Initialize Passport for authentication
 app.use(passport.initialize())
 require('./config/passport')(passport)
 
+// API Routes
 app.use('/animals', animalRoutes)
 app.use('/adopters', adopterRoutes)
 app.use('/adoptions', adoptionRoutes)
 app.use('/auth', authRoutes)
 
-app.listen(port, () => {
-  console.log(`Backend running on http://localhost:${port}`)
-})
+// Export Express app as serverless function
+module.exports = app
+module.exports.handler = serverless(app)
+
+// app.listen(port, () => {
+//   console.log(`Backend running on http://localhost:${port}`)
+// })
